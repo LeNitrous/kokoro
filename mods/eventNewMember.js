45c54f2645da50574451a204fce5b643ce87fb4e
LeNitrous <n20gaming2000@gmail.com>
2018-03-09 16:45:58 +0800

    some updates

--- mods/eventNewMember.js
+++ mods/eventNewMember.js
@@ -7,13 +7,16 @@ module.exports = {
                 .send(`Welcome ${member.toString()} to **Live Mapping Project**!` +
                       `\n` +
                       `\nYou are currently in probation to look over the server rules.` +
-                      `\nYou can read them at <#421546926803124224>. Please wait patiently as it will expire in 10 minutes.` +
-                      `\nWith that said, thank you for joining our community. This has been Kokoro, Happy! Lucky! Smile! Hooray!`);
+                      `\nYou can read them at <#421546926803124224>. Please wait patiently as it will expire in 5 minutes.` +
+                      `\nPlease follow them as they will be your guide and lifeline to this server.` +
+                      `\n` +
+                      `\nThis has been Kokoro, Happy! Lucky! Smile! Hooray!`);
             member.addRole(member.guild.roles.find("name", "Limited"), "A new member joined.");
             setTimeout(() => {
                 if (member) {
-                    member.send("You can now talk. Have fun!!!");
-                    member.removeRole(member.guild.roles.find("name", "Limited"), "Probation expired.")
+                    member.send("Your probation has ended and you can now talk. Don't forget to say hi to us!");
+                    member.removeRole(member.guild.roles.find("name", "Limited"), "Probation expired.");
+                    member.addRole(member.guild.roles.find("name", "Member"), "Probation expired.");
                 }
             }, 300000);
         }
