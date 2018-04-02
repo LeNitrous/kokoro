39271a7849c81634862ab0c4ccda25e5b469e6a1
LeNitrous <n20gaming2000@gmail.com>
2018-04-02 17:57:47 +0800

    fix typo

--- mods/events/messageUpdate.js
+++ mods/events/messageUpdate.js
@@ -5,7 +5,7 @@ module.exports = {
         if (oldMessage.guild.id == "261878898290196491") {
             // Guild Logs
             oldMessage.guild.channels.get("430280449668153365")
-                .send(`\`[${new Date().toLocaleTimeString()}]\` **[MESSAGE UPDATED]** ${oldMessage.channel.toString()} 🔁 __${oldMessage.member.user.tag}__ edited a message.\n▪ **Before:** ${oldMessage.content}\n▪ **Before:** ${newMessage.content}`);
+                .send(`\`[${new Date().toLocaleTimeString()}]\` **[MESSAGE UPDATED]** ${oldMessage.channel.toString()} 🔁 __${oldMessage.member.user.tag}__ edited a message.\n▪ **Before:** "${oldMessage.content}"\n▪ **After:** "${newMessage.content}"`);
         }
     }
 }
